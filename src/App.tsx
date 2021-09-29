70d44f1d2bfcadb731e22f30c756caf9bbb09e0e
Eduardo Silva <eduardolima1998@gmail.com>
2021-09-29 16:12:35 -0300

    feat: add all characters from api to local storage with apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
 import { Router } from './routes/app.routes';
 import { ChakraProvider } from '@chakra-ui/react';
+import { useQuery } from '@apollo/client';
 
 import { theme } from './styles/theme';
 import './styles/scrollbarStyles.css';
 
+import { GET_CHARACTERS_BY_IDS } from './apollo/queries';
+
 export function App() {
+  const allStoragedCharacters: Array<any> = JSON.parse(
+    localStorage.getItem('allCharacters') || '[]'
+  );
+
+  const doQuery = allStoragedCharacters.length === 0;
+
+  let i = 0;
+  let array = [];
+
+  while (i <= 660) {
+    array.push(i);
+    i++;
+  }
+
+  const { error, loading, data } = useQuery(GET_CHARACTERS_BY_IDS, {
+    skip: !doQuery,
+    variables: {
+      characterIds: array,
+    },
+  });
+
+  if (loading) return <p>Loading... </p>;
+  if (error) return <p>Error :( </p>;
+
+  if (doQuery) {
+    localStorage.setItem('allCharacters', JSON.stringify(data.charactersByIds));
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Router />
