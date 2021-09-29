6bac9fdfcfe89744194846f57617edf4fb52797f
Eduardo Silva <eduardolima1998@gmail.com>
2021-09-29 18:27:13 -0300

    feat(routes): add all pages and searchbar to app routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,21 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { Searchbar } from '../components/Searchbar';
+import { Home } from '../pages/Home';
+import { CharacterDetails } from '../pages/CharacterDetails';
 
 export function Router() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Searchbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/character/:id">
+          <CharacterDetails />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
