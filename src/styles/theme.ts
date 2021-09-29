356f24dbf0da43a7a31bb9095612df3ce9e84e0e
Eduardo Silva <eduardolima1998@gmail.com>
2021-09-29 15:57:24 -0300

    feat(styles): add global theme with chakra

---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
+
+const breakpoints = createBreakpoints({
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1536px',
+});
+
+export const theme = extendTheme({
+  colors: {
+    active: '#38B2AC',
+  },
+  breakpoints,
+  fonts: {
+    heading: 'RobotoLight',
+    body: 'RobotoLight',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.50',
+      },
+    },
+  },
+});
