877f48e74bd9ac24ee6bb9d1c5a38d064fb3f639
Eduardo Silva <eduardolima1998@gmail.com>
2021-10-01 22:13:51 -0300

    feat(utils): add helpers

---
+++ src/utils/fakeCharactersList.js
@@ -0,0 +1,288 @@
+export const fakeCharactersList = [
+  {
+    id: '1',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+    name: 'Rick Sanchez',
+    status: 'Alive',
+    species: 'Human',
+    origin: {
+      name: 'Earth (C-137)',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+    },
+  },
+  {
+    id: '2',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg',
+    name: 'Morty Smith',
+    status: 'Alive',
+    species: 'Human',
+    origin: {
+      name: 'Earth (C-137)',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+    },
+  },
+  {
+    id: '3',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg',
+    name: 'Summer Smith',
+    status: 'Alive',
+    species: 'Human',
+    origin: {
+      name: 'Earth (Replacement Dimension)',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+    },
+  },
+  {
+    id: '4',
+    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg',
+    name: 'Beth Smith',
+    status: 'Alive',
+    species: 'Human',
+    origin: {
+      name: 'Earth (Replacement Dimension)',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+    },
+  },
+  {
+    id: '5',
+    image: 'https://rickandmortyapi.com/api/character/avatar/5.jpeg',
+    name: 'Jerry Smith',
+    status: 'Alive',
+    species: 'Human',
+    origin: {
+      name: 'Earth (Replacement Dimension)',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+    },
+  },
+  {
+    id: '6',
+    image: 'https://rickandmortyapi.com/api/character/avatar/6.jpeg',
+    name: 'Abadango Cluster Princess',
+    status: 'Alive',
+    species: 'Alien',
+    origin: {
+      name: 'Abadango',
+    },
+    location: {
+      name: 'Abadango',
+    },
+  },
+  {
+    id: '7',
+    image: 'https://rickandmortyapi.com/api/character/avatar/7.jpeg',
+    name: 'Abradolf Lincler',
+    status: 'unknown',
+    species: 'Human',
+    origin: {
+      name: 'Earth (Replacement Dimension)',
+    },
+    location: {
+      name: 'Testicle Monster Dimension',
+    },
+  },
+  {
+    id: '8',
+    image: 'https://rickandmortyapi.com/api/character/avatar/8.jpeg',
+    name: 'Adjudicator Rick',
+    status: 'Dead',
+    species: 'Human',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'Citadel of Ricks',
+    },
+  },
+  {
+    id: '9',
+    image: 'https://rickandmortyapi.com/api/character/avatar/9.jpeg',
+    name: 'Agency Director',
+    status: 'Dead',
+    species: 'Human',
+    origin: {
+      name: 'Earth (Replacement Dimension)',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+    },
+  },
+  {
+    id: '10',
+    image: 'https://rickandmortyapi.com/api/character/avatar/10.jpeg',
+    name: 'Alan Rails',
+    status: 'Dead',
+    species: 'Human',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: "Worldender's lair",
+    },
+  },
+  {
+    id: '11',
+    image: 'https://rickandmortyapi.com/api/character/avatar/11.jpeg',
+    name: 'Albert Einstein',
+    status: 'Dead',
+    species: 'Human',
+    origin: {
+      name: 'Earth (C-137)',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+    },
+  },
+  {
+    id: '12',
+    image: 'https://rickandmortyapi.com/api/character/avatar/12.jpeg',
+    name: 'Alexander',
+    status: 'Dead',
+    species: 'Human',
+    origin: {
+      name: 'Earth (C-137)',
+    },
+    location: {
+      name: 'Anatomy Park',
+    },
+  },
+  {
+    id: '13',
+    image: 'https://rickandmortyapi.com/api/character/avatar/13.jpeg',
+    name: 'Alien Googah',
+    status: 'unknown',
+    species: 'Alien',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+    },
+  },
+  {
+    id: '14',
+    image: 'https://rickandmortyapi.com/api/character/avatar/14.jpeg',
+    name: 'Alien Morty',
+    status: 'unknown',
+    species: 'Alien',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'Citadel of Ricks',
+    },
+  },
+  {
+    id: '15',
+    image: 'https://rickandmortyapi.com/api/character/avatar/15.jpeg',
+    name: 'Alien Rick',
+    status: 'unknown',
+    species: 'Alien',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'Citadel of Ricks',
+    },
+  },
+  {
+    id: '16',
+    image: 'https://rickandmortyapi.com/api/character/avatar/16.jpeg',
+    name: 'Amish Cyborg',
+    status: 'Dead',
+    species: 'Alien',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'Earth (Replacement Dimension)',
+    },
+  },
+  {
+    id: '17',
+    image: 'https://rickandmortyapi.com/api/character/avatar/17.jpeg',
+    name: 'Annie',
+    status: 'Alive',
+    species: 'Human',
+    origin: {
+      name: 'Earth (C-137)',
+    },
+    location: {
+      name: 'Anatomy Park',
+    },
+  },
+  {
+    id: '18',
+    image: 'https://rickandmortyapi.com/api/character/avatar/18.jpeg',
+    name: 'Antenna Morty',
+    status: 'Alive',
+    species: 'Human',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'Citadel of Ricks',
+    },
+  },
+  {
+    id: '19',
+    image: 'https://rickandmortyapi.com/api/character/avatar/19.jpeg',
+    name: 'Antenna Rick',
+    status: 'unknown',
+    species: 'Human',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'unknown',
+    },
+  },
+  {
+    id: '20',
+    image: 'https://rickandmortyapi.com/api/character/avatar/20.jpeg',
+    name: 'Ants in my Eyes Johnson',
+    status: 'unknown',
+    species: 'Human',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'Interdimensional Cable',
+    },
+  },
+  {
+    id: '21',
+    image: 'https://rickandmortyapi.com/api/character/avatar/21.jpeg',
+    name: 'Aqua Morty',
+    status: 'unknown',
+    species: 'Humanoid',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'Citadel of Ricks',
+    },
+  },
+  {
+    id: '22',
+    image: 'https://rickandmortyapi.com/api/character/avatar/22.jpeg',
+    name: 'Aqua Rick',
+    status: 'unknown',
+    species: 'Humanoid',
+    origin: {
+      name: 'unknown',
+    },
+    location: {
+      name: 'Citadel of Ricks',
+    },
+  },
+];
